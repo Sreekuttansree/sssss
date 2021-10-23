45cdd688f4684a523be2b858e098d3475dc9a47f
Sreekuttansree <88297456+Sreekuttansree@users.noreply.github.com>
2021-10-23 21:22:41 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = f"{file_full_name}",
+                caption ={file_name}",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
